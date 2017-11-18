5bc97346133b4899ee7ee93ad298cf16e6d7699a
Marta <martadabrowka@gmail.com>
2017-11-18 12:58:57 +0000

    passed can add and remove guests from the table tests

--- app/src/main/java/com/example/marta/mariosrestaurant/tables/Table.java
+++ app/src/main/java/com/example/marta/mariosrestaurant/tables/Table.java
@@ -1,8 +1,31 @@
 package com.example.marta.mariosrestaurant.tables;
 
+import java.util.ArrayList;
+
 /**
  * Created by marta on 18/11/2017.
  */
 
-public class Table {
+public class Table<Patron> {
+    ArrayList<Patron> table;
+
+
+    public Table() {
+        this.table = new ArrayList();
+    }
+
+    public int tableSize() {
+        return this.table.size();
+    }
+
+
+    public void addGuest(Patron patron) {
+        table.add(patron);
+    }
+
+    public void removeGuest(Patron patron) {
+        table.remove(patron);
+    }
 }
+
+
--- app/src/test/java/com/example/marta/mariosrestaurant/tablestest/TableTest.java
+++ app/src/test/java/com/example/marta/mariosrestaurant/tablestest/TableTest.java
@@ -1,8 +1,51 @@
 package com.example.marta.mariosrestaurant.tablestest;
 
+import com.example.marta.mariosrestaurant.tables.Patron;
+import com.example.marta.mariosrestaurant.tables.Table;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by marta on 18/11/2017.
  */
 
 public class TableTest {
+    Patron patron1, patron2, patron3;
+    Table table101;
+
+    @Before
+    public void before() {
+        patron1 = new Patron("Terry", 300);
+        patron2 = new Patron("Frankie", 400);
+        patron3 = new Patron("Rory", 100);
+        table101 = new Table();
+    }
+
+
+    @Test
+    public void testTableStartsEmpty() {
+        assertEquals(0, table101.tableSize());
+    }
+
+    @Test
+    public void testCanAddGuestToTable() {
+        table101.addGuest(patron1);
+        assertEquals(1, table101.tableSize());
+    }
+
+    @Test
+    public void testCanRemoveGuestFromTable() {
+        table101.addGuest(patron1);
+        table101.addGuest(patron2);
+        table101.addGuest(patron3);
+        table101.removeGuest(patron3);
+        assertEquals(2, table101.tableSize());
+    }
+
+
 }
