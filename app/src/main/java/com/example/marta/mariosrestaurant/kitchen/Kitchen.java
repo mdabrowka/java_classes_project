57fc7f84904c6ebf6194a2ecc7c41f2b0b60899f
Marta <martadabrowka@gmail.com>
2017-11-20 16:11:31 +0000

    added a hashmap to the pantry

--- app/src/main/java/com/example/marta/mariosrestaurant/kitchen/Kitchen.java
+++ app/src/main/java/com/example/marta/mariosrestaurant/kitchen/Kitchen.java
@@ -1,5 +1,7 @@
 package com.example.marta.mariosrestaurant.kitchen;
 
+import com.example.marta.mariosrestaurant.dishes.Dish;
+
 /**
  * Created by marta on 20/11/2017.
  */
@@ -12,5 +14,14 @@ public class Kitchen {
         this.kitchen = new Kitchen();
 
     }
+
+//    public Dish prepareDish(String name) {
+//        dish = new Dish("dish")
+//}
+    // new up an instance of a dish class based on the name given? eg. "tomato pasta" passed in and
+    // kitchen makes tomatoPasta = new Dish("tomato pasta")???
+    // dish ingredients array starts off empty
+    //loop through the pantry and select items necessary to make the dish
+    //
 
 }
--- app/src/main/java/com/example/marta/mariosrestaurant/kitchen/Pantry.java
+++ app/src/main/java/com/example/marta/mariosrestaurant/kitchen/Pantry.java
@@ -1,19 +1,23 @@
 package com.example.marta.mariosrestaurant.kitchen;
 
 import com.example.marta.mariosrestaurant.dishes.Ingredient;
+import com.example.marta.mariosrestaurant.dishes.MenuItem;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by marta on 20/11/2017.
  */
 
 public class Pantry {
-    private ArrayList<Ingredient> pantry;
+    //private ArrayList<Ingredient> pantry;
+    private HashMap<MenuItem, Integer> stock;
 
 
     public Pantry() {
-        this.pantry = new ArrayList;
+       // this.pantry = new ArrayList<Ingredient>();
+        this.stock = new HashMap<MenuItem, Integer>();
     }
 
 }
