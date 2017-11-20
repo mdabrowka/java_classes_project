110cc04b9f91c96ffed30a6327ddf3163215eca1
Marta <martadabrowka@gmail.com>
2017-11-20 11:58:32 +0000

    started on menu methods

--- app/src/main/java/com/example/marta/mariosrestaurant/dishes/Menu.java
+++ app/src/main/java/com/example/marta/mariosrestaurant/dishes/Menu.java
@@ -9,4 +9,28 @@ import java.util.ArrayList;
 public class Menu<Menuable> {
     private ArrayList<Menuable> menuOptions;
 
+
+
+    public int dishCount() {
+      return menuOptions.size();
+    }
+
+
+    public void addToTheMenu(Menuable item) {
+        menuOptions.add(item);
+    }
+
+    public void removeFromTheMenu(Menuable item) {
+        menuOptions.remove(item);
+    }
+
+
+    public boolean checkIFSthIsInIt(Menuable item) {
+       if menuOptions.contains(Menuable item) == true;
+       return
+
+    }
+
+
+
 }
